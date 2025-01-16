000cf0142afcf95d739ea1690674ddf1a35e2c03
Mark Day <30710565+maaarkDay@users.noreply.github.com>
2025-01-16 10:37:41 -0500

    Create init.lua

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,9 @@
+return {
+  require("plugins.colorscheme"),
+  { "folke/lazy.nvim" },
+  require("plugins.autopairs"),
+  require("plugins.telescope"),
+  require("plugins.treesitter"),
+  require("plugins.lsp"),
+  require("plugins.copilot"),
+}
